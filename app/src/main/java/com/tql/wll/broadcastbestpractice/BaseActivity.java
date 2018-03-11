0d952e37a97f359b37a08c2d3e2db559b42de006
thenew01 <9091131@qq.com>
2018-03-11 19:26:57 +0800

    add broadcast to force offline

--- app/src/main/java/com/tql/wll/broadcastbestpractice/BaseActivity.java
+++ app/src/main/java/com/tql/wll/broadcastbestpractice/BaseActivity.java
@@ -1,5 +1,12 @@
 package com.tql.wll.broadcastbestpractice;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -8,6 +15,9 @@ import android.util.Log;
  */
 
 public class BaseActivity extends AppCompatActivity {
+
+    private ForceOfflineReceiver forceOfflineReceiver;
+
     @Override
     protected  void  onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
@@ -20,5 +30,42 @@ public class BaseActivity extends AppCompatActivity {
     protected void onDestroy(){
         super.onDestroy();
         ActivityCollector.removeActivity(this);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("FORCE_OFFLINE");
+        forceOfflineReceiver = new ForceOfflineReceiver();
+        registerReceiver(forceOfflineReceiver, intentFilter);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        if( forceOfflineReceiver != null ){
+            unregisterReceiver(forceOfflineReceiver);
+            forceOfflineReceiver = null;
+        }
+    }
+
+    class ForceOfflineReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(final Context context, Intent intent){
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Warning");
+            builder.setMessage("You are force offline, please try to login again");
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCollector.finishAll();
+                    Intent intent1 = new Intent(context, LoginActivity.class);
+                    startActivity(intent1);
+                }
+            });
+            builder.show();
+        }
     }
 }
--- app/src/main/java/com/tql/wll/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/com/tql/wll/broadcastbestpractice/LoginActivity.java
@@ -3,6 +3,7 @@ package com.tql.wll.broadcastbestpractice;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -333,8 +334,10 @@ public class LoginActivity extends BaseActivity implements LoaderCallbacks<Curso
             showProgress(false);
 
             if (success) {
+                //finish();
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
                 finish();
-
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
